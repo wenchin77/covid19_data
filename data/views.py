a92f1a9fe8c297009febc296e81fb95ad8a6f517
Wenchin <wenchin.ch@gmail.com>
2020-04-04 17:04:30 +0800

    Iterated through the data by country

--- data/views.py
+++ data/views.py
@@ -1,18 +1,15 @@
 from django.shortcuts import render
-
-# Create your views here.
 from django.http import HttpResponse
+from datetime import datetime
 import requests
 
-
-# def index(request):
-#     return HttpResponse("Hello, world. You're at the data index.")
-
-
 def index(request):
-    response = requests.get('https://pomber.github.io/covid19/timeseries.json')
-    data = response.json()
-    return render(request, 'data/index.html', {
-        'date': data,
-        # 'confirmed': data['Thailand']
-    })
+  response = requests.get('https://pomber.github.io/covid19/timeseries.json')
+  data = response.json()
+  country = 'Australia'
+  lastdate = len(data[country])-1
+  print(data[country][lastdate]['confirmed'])
+  return render(request, 'data/index.html', {
+    'date': data[country][lastdate]['date'],
+    'data': data,
+  })
